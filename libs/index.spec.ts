4043635b328e0e4884398ccb2ff7bcafa611f132
daisuke kurata <d.kruata.38@gmail.com>
2022-09-21 18:27:21 +0900

    塗る必要があるマス目の数と行全体のマス目の数をもとに、確定マスを計算する関数を実装

--- libs/index.spec.ts
+++ libs/index.spec.ts
@@ -1,4 +1,4 @@
-import { requiredPixelSize } from "."
+import {pixelStatuses, requiredPixelSize} from "."
 
 describe('requiredPixelSize', () => {
     it('塗りつぶす箇所が0個だったら0を返す', () => {
@@ -14,5 +14,27 @@ describe('requiredPixelSize', () => {
     it('塗りつぶす箇所が2個以上だったら塗りつぶすマスの間の空白マスが1個と仮定して計算する', () => {
         expect(requiredPixelSize([2, 5, 10]))
             .toBe(19)
+    })
+})
+
+describe('pixelStatuses', () => {
+    it('塗りつぶす箇所が0個だったら全部のマス目を空で埋めた配列を返す', () => {
+        expect(pixelStatuses(5, []))
+            .toEqual(["blank", "blank", "blank", "blank", "blank"])
+    })
+
+    it('全部のマス目が確定マスになる場合', () => {
+        expect(pixelStatuses(5, [1, 3]))
+            .toEqual(["painted", "blank", "painted", "painted", "painted"])
+    })
+
+    it('不定マスがある場合', () => {
+        expect(pixelStatuses(5, [2, 1]))
+            .toEqual(["unknown", "painted", "unknown", "unknown", "unknown"])
+    })
+
+    it('不定マスしかない場合', () => {
+        expect(pixelStatuses(5, [1, 1]))
+            .toEqual(["unknown", "unknown", "unknown", "unknown", "unknown"])
     })
 })
