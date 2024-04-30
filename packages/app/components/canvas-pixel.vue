0e16baa607e586af6da24ccde29f58ac434c0212
Kuchita El <el.kuchita@gmail.com>
2024-04-30 21:53:53 +0900

    #8 Canvas のマスを表すコンポーネントを実装

---
+++ packages/app/components/canvas-pixel.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const props = defineProps<{ state: "empty" | "filled" | "TBD" }>()
+</script>
+
+<template>
+  <div class="canvas-pixel" :class="{ 'filled-pixel': props.state === 'filled', 'empty-pixel': props.state === 'empty' }"/>
+</template>
+
+<style scoped>
+.canvas-pixel {
+  aspect-ratio: 1 / 1;
+  border: black 1px solid;
+}
+
+.filled-pixel {
+  background-color: dimgray;
+}
+
+.empty-pixel {
+  background-image: linear-gradient(to top left, transparent, transparent 46%, dimgray 46%, dimgray 56%, transparent 53%, transparent);
+}
+
+</style>
